01a7c563d9f6eb4c05a60ccaaeb761dadd753d12
Vi-Souza <vitorsr.souza02@gmail.com>
2026-05-14 18:33:01 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/projetofeitv/ProjetoFEITV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetofeitv;
+
+/**
+ *
+ * @author Vitor
+ */
+public class ProjetoFEITV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
