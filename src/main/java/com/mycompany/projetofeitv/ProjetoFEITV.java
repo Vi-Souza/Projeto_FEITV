595f88ef92d5884c049d0f8ad21348ee7d63e703
Vi-Souza <vitorsr.souza02@gmail.com>
2026-05-14 19:39:45 -0300

    View de Login, Playlist e Login, com Controller

--- src/main/java/com/mycompany/projetofeitv/ProjetoFEITV.java
+++ src/main/java/com/mycompany/projetofeitv/ProjetoFEITV.java
@@ -1,35 +1,24 @@
 package com.mycompany.projetofeitv;
 
-import com.mycompany.projetofeitv.model.Video;
-import com.mycompany.projetofeitv.controller.VideoController;
 /**
  * Classe principal do projeto FEItv.
  * Aqui é onde o programa começa a ser executado.
  * 
  * @author Vitor
  */
+
+import com.mycompany.projetofeitv.controller.UsuarioController;
+import com.mycompany.projetofeitv.view.LoginView;
+
 public class ProjetoFEITV {
     public static void main(String[] args) {
-        VideoController controller = new VideoController();
-        
-        // Criando vídeos
-        Video v1 = new Video(1, "Aula de Java", "Introdução ao Java", "YouTube");
-        Video v2 = new Video(2, "Swing Tutorial", "Como criar GUIs", "YouTube");
-        
-        // Cadastrando vídeos
-        controller.cadastrarVideo(v1);
-        controller.cadastrarVideo(v2);
-        
-        // Listando vídeos
-        controller.listarVideos();
-        
-        // Buscando vídeo
-        Video encontrado = controller.buscarVideo("Swing Tutorial");
-        System.out.println("Vídeo encontrado: " + encontrado);
-        
-        // Removendo vídeo
-        controller.removerVideo(v1);
-        controller.listarVideos();
+        // Cria o controller de usuários
+        UsuarioController usuarioController = new UsuarioController();
+
+        // Abre a tela de login como ponto inicial do sistema
+        LoginView loginView = new LoginView(usuarioController);
+        loginView.setVisible(true);
     }
 }
+
 
